10dc1dce8b10e1ef1575c9937c2303a7f572559c
Lewis Scott <lewscott57@yahoo.co.uk>
2022-05-10 11:42:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Csv_Test
+{
+    class Program
+    {
+
+
+        
+
+
+        static void Main(string[] args)
+        {
+
+            int _numberOfEvents = 8;
+
+            string _filePath = "../";
+            string _fileName = @"GenericEvents.csv";
+            
+            string[,] _storage = new string[8, 26];
+
+
+            string[] _input = File.ReadAllLines(_filePath + _fileName);
+            
+
+
+            for (int line = 1; line <= _numberOfEvents; line ++)
+            {
+                int cell = 0;                
+
+                for (int character = 0; character < _input[line].Length; character++)
+                {
+                    if (_input[line][character] == ',')
+                    {
+                        cell++;
+                    }
+                    else if (_input[line][character] == '@')
+                    {
+                        _storage[line - 1, cell] += ',';
+                    }
+                    else if (_input[line][character] == '%')
+                    {
+                        _storage[line - 1, cell] += '"';
+                    }
+                    else 
+                    {
+                        _storage[line - 1, cell] += _input[line][character];
+                    }
+
+                }
+                
+            }
+
+            Console.WriteLine(_storage[4, 3]);
+        }
+    }
+}
